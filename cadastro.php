8f9fd38f59e66663cd04390f59fff179bb8b3a6f
Bento-Hertz <bentoluiz50@gmail.com>
2022-06-14 12:22:11 -0500

    envio de dados para o BD

--- cadastro.php
+++ cadastro.php
@@ -1,4 +1,18 @@
 <?php
-    include(conexao.php);
+include("./conexao.php");
+
+$nome = $_POST["nome"];
+$data = $_POST["data"];
+$email = $_POST["email"];
+$senha = $_POST["senha"];
+
+$sql = "INSERT INTO usuarios(`nome`, `nascimento`, `email`, `senha`) Values ('$nome', '$data', '$email', '$senha')";
+
+if(mysqli_query($conexao, $sql))
+    echo "Cadastro realizado com sucesso.";
+else
+    echo "Erro! " . mysqli_connect_error($conexao);
+
+mysqli_close($conexao);
 
 ?>
--- enviar.php
+++ enviar.php
@@ -1,8 +1,10 @@
 <?php
+/*
 $nome = $_POST["nome"];
 $data = $_POST["data"];
 $email = $_POST["email"];
 $senha = $_POST["senha"];
 
 echo "Dados: (" . $nome . ", " . $data . ", " . $email . ", " . $senha . ")";
+*/
 ?>

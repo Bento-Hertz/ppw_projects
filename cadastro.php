2593a944df39d83a34f628c896d16632610d43b0
Bento-Hertz <bentoluiz50@gmail.com>
2022-06-14 11:54:13 -0500

    alteração

--- cadastro.php
+++ cadastro.php
@@ -1,4 +1,4 @@
 <?php
-include(conexao.php);
+    include(conexao.php);
 
 ?>
---
+++ enviar.php
@@ -0,0 +1,8 @@
+<?php
+$nome = $_POST["nome"]
+$data = $_POST["data"]
+$email = $_POST["email"]
+$senha = $_POST["senha"]
+
+echo "Dados: (" . $nome . ", " . $data . ", " . $email . ", " . $senha . ")"
+?>

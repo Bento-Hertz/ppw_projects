ec79723b08f2f429b273421d10f590b6cee1a07d
Bento-Hertz <bentoluiz50@gmail.com>
2022-06-16 22:39:23 -0500

    alteração

--- enviar.php
+++ enviar.php
@@ -1,10 +1,5 @@
 <?php
 /*
-$nome = $_POST["nome"];
-$data = $_POST["data"];
-$email = $_POST["email"];
-$senha = $_POST["senha"];
-
-echo "Dados: (" . $nome . ", " . $data . ", " . $email . ", " . $senha . ")";
+teste
 */
 ?>
